5b3a630898f7e702706c5f6797c502dc33a54925
Maria Fernanda <maria.salomao@ufn.edu.br>
2023-04-05 19:00:31 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.WriteLine("Informe um numero: ");
+            n1 = Convert.ToDouble(Console.ReadLine());  
+
+            if (n1 % 2 ==  0)
+            {
+                Console.WriteLine("Numero par");
+            }
+            else
+            {
+                Console.WriteLine("Numero impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
